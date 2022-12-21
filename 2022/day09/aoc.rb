005ed737ea05a5a44310253c7d021589694f9362
Sebastian Cohnen <tisba@users.noreply.github.com>
2022-12-21 19:10:10 +0100

    2022/ruby: adds day 9 part 2

--- 2022/day09/aoc.rb
+++ 2022/day09/aoc.rb
@@ -6,15 +6,15 @@ class AoC; end
 
 class AoC::Day09
   class << self
-    def call(input:)
+    def call(input:, segment_count:)
       motions = input.each_line.map do |line|
         direction, length = line.split(" ")
 
         vector = case direction
-               when "U"  then [ 0, -1]
-               when "R"  then [ 1,  0]
-               when "D"  then [ 0,  1]
-               when "L"  then [-1,  0]
+               when "U" then [ 0, -1]
+               when "R" then [ 1,  0]
+               when "D" then [ 0,  1]
+               when "L" then [-1,  0]
                end
 
         [ vector, length.to_i ]
@@ -22,56 +22,41 @@ class AoC::Day09
 
       tail_visited = Set.new
 
-      head = [0, 0]
-      tail = [0, 0]
+      head = [0, 4]
+      segments = [[0, 4]] * segment_count
 
-      tail_visited.add(tail)
+      tail_visited.add(segments.last)
 
       motions.each do |vector, length|
         length.times do
           head = move(head, vector)
-          tail = update_tail(tail, head)
+          segments = segments.each_with_object([head]) do |segment, acc|
+            acc << update_segment(segment, acc.last)
+          end
+          segments.shift
 
-          tail_visited.add(tail)
+          tail_visited.add(segments.last)
         end
       end
 
-      [tail_visited.count, nil]
+      tail_visited.count
     end
 
-    def update_tail(tail, head)
-      return tail if tail == head
+    def update_segment(segment, prev_segment)
+      dy = prev_segment[1] - segment[1]
+      dx = prev_segment[0] - segment[0]
 
-      tx, ty = tail
-      hx, hy = head
+      # no movement, segment is adjacent to previous
+      # segment ("head")
+      if   (dx == 0 && dy.abs <= 1) \
+        || (dy == 0 && dx.abs <= 1) \
+        || (dx.abs < 2 && dy.abs < 2)
+        segment
 
-      if tx == hx && (ty - hy).abs > 1
-        if ty > hy
-          return move(tail, [0, -1]) # up
-        else
-          return move(tail, [0, 1]) # down
-        end
+      else
+        # move into the direction of the deltas
+        move(segment, [ dx <=> 0, dy <=> 0 ])
       end
-
-      if ty == hy && (tx - hx).abs > 1
-        if tx > hx
-          return move(tail, [-1, 0]) # left
-        else
-          return move(tail, [1, 0]) # right
-        end
-      end
-
-      if ty != hy && tx != hx
-        x = [hx - tx, hy - ty]
-        return tail if x[0].abs < 2 && x[1].abs < 2
-
-        x[0] -= x[0] <=> 0 if x[0].abs >= 2
-        x[1] -= x[1] <=> 0 if x[1].abs >= 2
-
-        return move(tail, x)
-      end
-
-      tail
     end
 
     def move(coord, vector)
@@ -94,14 +79,60 @@ require "rspec/autorun"
 
 RSpec.describe AoC::Day09 do
   context "with test input" do
-    subject { described_class.call(input: Pathname.new(__dir__).join("input_test.txt")) }
+    subject { described_class.call(input: input, segment_count: segment_count) }
 
-    it { expect(subject).to eq([13, nil]) }
+    context "for part one" do
+      let(:segment_count) { 1 }
+
+      let(:input) do
+        <<~EOS
+        R 4
+        U 4
+        L 3
+        D 1
+        R 4
+        D 1
+        L 5
+        R 2
+        EOS
+      end
+
+      it { expect(subject).to eq(13) }
+    end
+
+    context "for part two" do
+      let(:segment_count) { 9 }
+
+      let(:input) do
+        <<~EOS
+        R 5
+        U 8
+        L 8
+        D 3
+        R 17
+        D 10
+        L 25
+        U 20
+        EOS
+      end
+
+      it { expect(subject).to eq(36) }
+    end
   end
 
   context "with input" do
-    subject { described_class.call(input: Pathname.new(__dir__).join("input.txt")) }
+    subject { described_class.call(input: Pathname.new(__dir__).join("input.txt"), segment_count: segment_count) }
 
-    it { expect(subject).to eq([6384, nil]) }
+    context "for part one" do
+      let(:segment_count) { 1 }
+
+      it { expect(subject).to eq(6384) }
+    end
+
+    context "for part two" do
+      let(:segment_count) { 9 }
+
+      it { expect(subject).to eq(2734) }
+    end
   end
 end
