894cae255378d8009db80bfb7b82eb10a1148dc0
Sebastian Cohnen <tisba@users.noreply.github.com>
2022-12-29 10:03:53 +0100

    ruby: adds template file

---
+++ 2022/aoc_template.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+require "pathname"
+require "json"
+
+class AoC; end
+
+class AoC::DayXX
+  class << self
+    def call(input:)
+    end
+  end
+end
+
+require "bundler/inline"
+
+gemfile do
+  source "https://rubygems.org"
+  gem "rspec"
+end
+
+require "rspec/autorun" if Pathname.new($0).basename.to_s != "rspec"
+
+RSpec.describe AoC::DayXX do
+  context "with test input" do
+    subject { described_class.call(input: Pathname.new(__dir__).join("input_test.txt")) }
+
+    it { expect(subject).to eq([nil, nil]) }
+  end
+
+  context "with input" do
+    subject { described_class.call(input: Pathname.new(__dir__).join("input.txt")) }
+
+    it { expect(subject).to eq([nil, nil]) }
+  end
+end
