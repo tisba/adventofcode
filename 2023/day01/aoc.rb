c8a80984e03d27dc37c8d03a5abf8991344f42e2
Sebastian Cohnen <tisba@users.noreply.github.com>
2023-12-19 13:47:40 +0100

    2023/ruby: day 1, part 2 (finally!)

--- 2023/day01/aoc.rb
+++ 2023/day01/aoc.rb
@@ -7,10 +7,29 @@ class AoC; end
 
 class AoC::Puzzle
   class << self
-    def call(input:)
-      input.each_line.reduce(0) do |sum, line|
-        numbers = line.strip.each_char.select { |c| c.match?(/\d/) }
-        sum += (numbers.first + numbers[-1]).to_i
+    def call(input:, with_words: false)
+      words = Hash[(1..9).map { |i| [i, i] }]
+
+      if with_words
+        words = words.merge(Hash[%w(one two three four five six seven eight nine).each_with_index.map { |n, i| [n, i+1] }])
+      end
+
+      words = words.transform_keys(&:to_s).transform_values(&:to_s)
+
+      scan_for = /#{words.keys.join("|")}/
+
+      input.each_line.inject(0) do |sum, line|
+        line.strip!
+
+        next if line.empty?
+
+        numbers = line.size.times.map do |i|
+          line[i..].match(scan_for)&.match(0)
+        end.compact
+
+        first, last = numbers[0], numbers[-1]
+
+        sum + (words[first] + words[last]).to_i
       end
     end
   end
@@ -38,14 +57,30 @@ end
 
 RSpec.describe AoC::Puzzle do
   context "with test input" do
-    subject { described_class.call(input: input_test) }
+    context "for part 1" do
+      subject { described_class.call(input: input_test, with_words: false) }
 
-    it { expect(subject).to eq(142) }
+      it { expect(subject).to eq(142) }
+    end
+
+    context "for part 2" do
+      subject { described_class.call(input: Pathname.new(__dir__).join("input_test2.txt"), with_words: true) }
+
+      it { expect(subject).to eq(281) }
+    end
   end
 
   context "with input" do
-    subject { described_class.call(input: input) }
+    context "for part 1" do
+      subject { described_class.call(input: input) }
 
-    it { expect(subject).to eq(54951) }
+      it { expect(subject).to eq(54951) }
+    end
+
+    context "for part 2" do
+      subject { described_class.call(input: input, with_words: true) }
+
+      it { expect(subject).to eq(55218) }
+    end
   end
 end
